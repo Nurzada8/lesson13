5ebe8b0ef5f48a28905d41be2813362ee347952c
nurzada <nurzadabakytkyzy8@gmail.com>
2022-05-09 11:07:41 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Triangle triangle = new Triangle();
+
+        double res = triangle.area(45.5, 7.4, 5.7);
+        System.out.println(res);
+
+
+    }
+
+    }
---
+++ src/Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle {
+
+    double a;
+    double b;
+    double c;
+
+    public double area(double a, double b, double c){
+        double res=(a+b+c)/2;
+        return res;
+    }
+
+
+
+}
